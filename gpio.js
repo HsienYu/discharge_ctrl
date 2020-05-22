97745dc97abdf69f6b0470e818cb0dfd3a4149d9
H.Y.C <chenghsienyu@aim.com>
2020-05-23 04:35:20 +0800

    add gpio

---
+++ gpio.js
@@ -0,0 +1,35 @@
+const PIN = require("rpi-pins");
+const exec = require('child_process').exec;
+const kill = require('tree-kill');
+
+const GPIO = new PIN.GPIO();
+GPIO.setPin(23, PIN.MODE.OUTPUT);
+GPIO.setPin(24, PIN.MODE.INPUT);
+GPIO.pullControl(24, PIN.MODE.PULL_UP);
+
+var sref = null;
+
+var stopRunningPlayer = function () {
+    if (sref && sref.pid > 0) {
+        kill(sref.pid, 'SIGTERM', function () {
+            myLog('Killed OMX player with PID: ', sref.pid);
+            sref = null;
+        });
+    }
+}
+
+
+
+if (GPIO.read(24)) {
+    //GPIO.write(27, PIN.MODE.HIGH);
+    stopRunningPlayer();
+    if (sref == null) {
+        var call = 'omxplayer' + `` //' --orientation 270 --aspect-mode stretch';
+        sref = exec(call);
+        sref.on('close', (code) => {
+            console.log('Finished');
+            stopRunningPlayer();
+            GPIO.write(23, PIN.MODE.HIGH);
+        });
+    }
+}
