55e5de1def3fe030086a2c1709bd0944ae7f5fd1
H.Y.C <chenghsienyu@aim.com>
2020-05-23 05:35:24 +0800

    add mqtt feature

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const mqtt = require('mqtt');
 const exec = require('child_process').exec;
-const { getInfo } = require('ytdl-getinfo')
-var youtube = require('youtube-random-video');
 const kill = require('tree-kill');
 const config = require('./config.json');
 
@@ -62,21 +60,6 @@ var stopAllPlayer = function () {
     }
 }
 
-var test = function () {
-    youtube.getRandomVid("AIzaSyBNeUrC8lz0OqOoljdB1xcB-kumH0I0cE4", function (err, data) {
-        //key is your youtube api key
-        //data is a JSON object
-        var obj = data.id;
-        var vid = obj.videoId;
-        var youtubeUrl = "https://www.youtube.com/watch?v=" + vid;
-        console.log(youtubeUrl);
-        var call = "omxplayer" + "`" + `youtube-dl -g ${youtubeUrl}` + "`";
-        console.log(call);
-        exec(call);
-    })
-}
-
-test();
 
 client.on('message', function (topic, message) {
     var action = topic.toString().split('/').pop();
@@ -88,18 +71,7 @@ client.on('message', function (topic, message) {
         case 'play-video':
             stopAllPlayer();
             if (sref == null) {
-                youtube.getRandomVid("AIzaSyBNeUrC8lz0OqOoljdB1xcB-kumH0I0cE4", function (err, data) {
-                    //key is your youtube api key
-                    //data is a JSON object
-                    var obj = data.id;
-                    var vid = obj.videoId;
-                    var youtubeUrl = "https://www.youtube.com/watch?v=" + vid;
-                    console.log(youtubeUrl);
-                    getInfo(youtubeUrl).then(info => {
-                        console.log(info.items[0].url)
-                    })
-                })
-                var call = 'omxplayer' + `` //' --orientation 270 --aspect-mode stretch';
+                var call = 'omxplayer' + ' ' + '/home/pi/synctest.mp4'  //' --orientation 270 --aspect-mode stretch';
                 sref = exec(call);
                 sref.on('close', (code) => {
                     console.log('Finished');
@@ -109,29 +81,29 @@ client.on('message', function (topic, message) {
             break;
         case 'play-audio':
             stopAllPlayer();
-            var call = 'omxplayer -o local ' + payload;
-            sref = exec(call);
+            //var call = 'omxplayer -o local ' + payload;
+            //sref = exec(call);
             break;
         case 'play-video-loop':
-            stopAllPlayer();
-            if (sref == null) {
-                var call = 'omxplayer -o local ' + payload + ' --orientation 270 --aspect-mode stretch';
-                sref = exec(call);
-                sref.on('close', (code) => {
-                    console.log('Finished');
-                    stopRunningPlayer();
-                });
-            }
+            // stopAllPlayer();
+            // if (sref == null) {
+            //     var call = 'omxplayer -o local ' + payload + ' --orientation 270 --aspect-mode stretch';
+            //     sref = exec(call);
+            //     sref.on('close', (code) => {
+            //         console.log('Finished');
+            //         stopRunningPlayer();
+            //     });
+            // }
             break;
         case 'stop-video':
         case 'stop-audio':
             stopAllPlayer();
             break;
         case 'volume-up':
-            if (sref) sref.stdin.write('+');
+            // if (sref) sref.stdin.write('+');
             break;
         case 'volume-down':
-            if (sref) sref.stdin.write('-');
+            // if (sref) sref.stdin.write('-');
             break;
     }
 });
